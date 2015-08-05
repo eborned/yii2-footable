654fac5605b43ed5dac47947850faa514db8191e
Eric Bus <eric.bus@websight.nl>
2015-08-05 12:29:06 +0200

    Wrong sourcePath for assets

--- FootableAsset.php
+++ FootableAsset.php
@@ -6,7 +6,7 @@ use \Yii;
 
 class FootableAsset extends \yii\web\AssetBundle
 {
-    public $sourcePath = '@vendor/filsh/footable';
+    public $sourcePath = '@vendor/bower/footable';
 
     public $js = [
         'js/footable.js'
